34862660ca15165af7e51c9d6efd04f2e492c521
mr19331 <mr19331@gmail.com>
2021-01-18 15:19:01 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/check_out.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IAAZJGUt7s8PIBIfIAg2TDlGMMunb65jyEl7UL6g3Zn5YUK5lxA2NyijN7hxibDNbgLaevadEtJWzs2eyqQqi7200C3jX52QA',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
