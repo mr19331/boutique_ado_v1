244bec62ddea144d795e813f81e8c61c47ce3474
mr19331 <mr19331@gmail.com>
2021-01-24 07:57:02 +0000

    added product form for product admin

---
+++ products/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms, Modelform):
+
+    class Meta:
+        models = Product
+        fields = '__all__'
+
+
+def __init__(self, *args, **kwargs):
+
+    super(), __intit__(*args, **kwargs)
+    categories = Category.objects.all()
+    friendly_name = [(c.id, c.get_friendly_name()) for c in categories]
+
+    self.fields['category'].choices = friendly_names
+    for field_name, field in self.fields.items():
+        field.widget.attrs['class'] = 'border-black rounded-0'
+    
